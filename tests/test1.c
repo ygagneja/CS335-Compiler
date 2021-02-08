7dcb4b7fecc4b66f106f1b7ec2749c7a0ba14f25
Yuvraj <yuvrajg@iitk.ac.in>
2021-02-08 15:12:41 +0530

    tests added

--- tests/test1.c
+++ tests/test1.c
@@ -1,3 +1,20 @@
 int main(){
- \t int
+    int i = 0;
+    for (i=0; i<10; i++){
+        if (i == 2)
+            printf ("hey there");
+    }
+    float a = 2;
+    while (a--){
+        break;
+    }
+    int a = 5+10;
+        int c = a;
+    float x = c&a; // one line comment
+      x += a - c<<5; 
+    /* multi 
+        line comment */
+    $$ = 6; // should throw error at line 17
+
+    return 0;
 }
--- tests/test3.c
+++ tests/test3.c
@@ -1,6 +1,15 @@
-int main(){
-int a = 2;
- int c = a>>5;
-    c <<= 2;
-	return 0;
+long int multiplyNumbers(int n);
+int main() {
+    int n;
+    printf("Enter a positive integer: ");
+    scanf("%d",&n);
+    printf("Factorial of %d = %ld", n, multiplyNumbers(n));
+    return 0;
+}
+
+long int multiplyNumbers(int n) {
+    if (n>=1)
+        return n*multiplyNumbers(n-1);
+    else
+        return 1;
 }
